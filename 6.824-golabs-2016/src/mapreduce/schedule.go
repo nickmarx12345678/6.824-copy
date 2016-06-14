324039c90f373c1c28687a62b01080d140a64092
Max Chen <maxchen675@gmail.com>
2016-06-13 23:02:25 -0700

    Lab 1.3: Distributed MapReduce.

--- 6.824-golabs-2016/src/mapreduce/schedule.go
+++ 6.824-golabs-2016/src/mapreduce/schedule.go
@@ -24,5 +24,43 @@ func (mr *Master) schedule(phase jobPhase) {
 	//
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 	//
+	// Register worker(s)
+	switch phase {
+	case mapPhase:
+		for fileIndex, fileName := range mr.files {
+			// Schedule work to be done:
+			mapArgs := DoTaskArgs{
+				JobName:       mr.jobName,
+				File:          fileName,
+				Phase:         phase,
+				TaskNumber:    fileIndex,
+				NumOtherPhase: nios,
+			}
+			// Wait for worker to be ready.
+			workerName := <-mr.registerChannel
+			// Schedule work for the worker.
+			go func() {
+				call(workerName, "Worker.DoTask", mapArgs, new(struct{}))
+				mr.registerChannel <- workerName
+			}()
+		}
+	case reducePhase:
+		for i := 0; i < ntasks; i++ {
+			// Schedule work to be done:
+			reduceArgs := DoTaskArgs{
+				JobName:       mr.jobName,
+				Phase:         phase,
+				TaskNumber:    i,
+				NumOtherPhase: nios,
+			}
+			// Wait for worker to be ready.
+			workerName := <-mr.registerChannel
+			// Schedule work for the worker.
+			go func() {
+				call(workerName, "Worker.DoTask", reduceArgs, new(struct{}))
+				mr.registerChannel <- workerName
+			}()
+		}
+	}
 	fmt.Printf("Schedule: %v phase done\n", phase)
 }
