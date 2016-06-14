42b6ec011ba43def7bab331e02b5ecbd2ef78135
Max Chen <maxchen675@gmail.com>
2016-06-13 23:02:25 -0700

    Initial ideas on MapReduce distribution.

--- 6.824-golabs-2016/src/mapreduce/schedule.go
+++ 6.824-golabs-2016/src/mapreduce/schedule.go
@@ -24,5 +24,31 @@ func (mr *Master) schedule(phase jobPhase) {
 	//
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 	//
+	// Register worker(s)
+	switch phase {
+	case mapPhase:
+		for fileIndex, fileName := range mr.files {
+			// Schedule work to be done:
+			mapArgs = new(DoTaskArgs)
+			mapArgs.JobName = mr.jobName
+			mapArgs.File = fileName
+			mapArgs.Phase = phase
+			mapArgs.TaskNumber = fileIndex
+			mapArgs.NumOtherPhase = nios
+			// TODO: Figure out how to get the worker's name.
+			_ := call(workerName, "Worker.DoTask", mapArgs, &response)
+		}
+	case reducePhase:
+		for i := 0; i < ntasks; i++ {
+			// Schedule work to be done:
+			reduceArgs = new(DoTaskArgs)
+			reduceArgs.JobName = mr.jobName
+			reduceArgs.Phase = phase
+			reduceArgs.TaskNumber = i
+			reduceArgs.NumOtherPhase = nios
+			// TODO: Figure out how to get the worker's name.
+			_ := call(workerName, "Worker.DoTask", reduceArgs, &response)
+		}
+	}
 	fmt.Printf("Schedule: %v phase done\n", phase)
 }
