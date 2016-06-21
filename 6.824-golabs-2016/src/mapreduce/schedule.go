62381c58d47657b0e12429e550699a6ad117add9
Max Chen <maxchen675@gmail.com>
2016-06-21 02:41:40 -0700

    Taking another stab at RPC/channels.

--- 6.824-golabs-2016/src/mapreduce/schedule.go
+++ 6.824-golabs-2016/src/mapreduce/schedule.go
@@ -2,7 +2,7 @@ package mapreduce
 
 import (
 	"fmt"
-	"strconv"
+	//"strconv"
 )
 
 // schedule starts and waits for all tasks in the given phase (Map or Reduce).
@@ -28,35 +28,36 @@ func (mr *Master) schedule(phase jobPhase) {
 	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
 	//
 	// Register worker(s)
-	fmt.Println(strconv.Itoa(len(mr.workers)))
-	for x := 0; x < len(mr.workers); x++ {
-		fmt.Println(mr.workers[x])
-	}
 	switch phase {
 	case mapPhase:
 		for fileIndex, fileName := range mr.files {
-			// Schedule work to be done:
-			mapArgs := new(DoTaskArgs)
-			mapArgs.JobName = mr.jobName
-			mapArgs.File = fileName
-			mapArgs.Phase = phase
-			mapArgs.TaskNumber = fileIndex
-			mapArgs.NumOtherPhase = nios
-			// TODO: Figure out how to get the worker's name.
-			workerName := ""
-			call(workerName, "Worker.DoTask", mapArgs, new(struct{}))
+				// Schedule work to be done:
+				mapArgs := new(DoTaskArgs)
+				mapArgs.JobName = mr.jobName
+				mapArgs.File = fileName
+				mapArgs.Phase = phase
+				mapArgs.TaskNumber = fileIndex
+				mapArgs.NumOtherPhase = nios
+				go func() {
+					workerName := <- mr.registerChannel
+					fmt.Println("MAP " + workerName)
+					call(workerName, "Worker.DoTask", mapArgs, new(struct{}))
+				}()
 		}
 	case reducePhase:
 		for i := 0; i < ntasks; i++ {
-			// Schedule work to be done:
-			reduceArgs := new(DoTaskArgs)
-			reduceArgs.JobName = mr.jobName
-			reduceArgs.Phase = phase
-			reduceArgs.TaskNumber = i
-			reduceArgs.NumOtherPhase = nios
-			// TODO: Figure out how to get the worker's name.
-			workerName := ""
-			call(workerName, "Worker.DoTask", reduceArgs, new(struct{}))
+				// Schedule work to be done:
+				reduceArgs := new(DoTaskArgs)
+				reduceArgs.JobName = mr.jobName
+				reduceArgs.Phase = phase
+				reduceArgs.TaskNumber = i
+				reduceArgs.NumOtherPhase = nios
+				//fmt.Println(reduceArgs.JobName)
+				go func() {
+					workerName := <- mr.registerChannel
+					call(workerName, "Worker.DoTask", reduceArgs, new(struct{}))
+					mr.registerChannel <- workerName
+				}()
 		}
 	}
 	fmt.Printf("Schedule: %v phase done\n", phase)
